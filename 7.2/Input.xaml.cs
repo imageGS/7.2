ef7ddb9bed5a48927782d9ebeee963b6c4132141
teddy <teddy@BEST-KOMP>
2024-07-03 17:06:04 +0300

    Добавьте файлы проекта.

---
+++ 7.2/Input.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _7._2
+{
+    /// <summary>
+    /// Логика взаимодействия для Input.xaml
+    /// </summary>
+    public partial class Input : Window
+    {
+        public Input()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(tbInput.Text))
+                {
+                    if (cbLenght.IsChecked == true & cbSquare.IsChecked == false)
+                    {
+                        MainWindow window = new MainWindow();
+                        window.Radius = Convert.ToDouble(tbInput.Text);
+                        window.IsCheckBoxSquare = false;
+                        window.IsCheckBoxLenght = true;
+                        window.Show();
+                        this.Close();
+                    }
+                    else if (cbSquare.IsChecked == true & cbLenght.IsChecked == false)
+                    {
+                        MainWindow window = new MainWindow();
+                        window.IsCheckBoxSquare = true;
+                        window.IsCheckBoxLenght = false;
+                        window.Radius = Convert.ToDouble(tbInput.Text);
+                        window.Show();
+                        this.Close();
+                    }
+                    else if (cbSquare.IsChecked == true & cbLenght.IsChecked == true)
+                    {
+                        MainWindow window = new MainWindow();
+                        window.IsCheckBoxSquare = true;
+                        window.IsCheckBoxLenght = true;
+                        window.Radius = Convert.ToDouble(tbInput.Text);
+                        window.Show();
+                        this.Close();
+                    }
+                    else MessageBox.Show("Please select one of the calculation options.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else MessageBox.Show("Please enter the radius.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter the correct radius.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ 7.2/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _7._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double Radius { get; set; }
+        public bool IsCheckBoxSquare { get; set; }
+        public bool IsCheckBoxLenght { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsCheckBoxLenght == true & IsCheckBoxSquare == false) MessageBox.Show($"Lenght of the circle = {Math.Round((2 * Math.PI * Radius),2)} cm", "Calculation");
+            else if (IsCheckBoxSquare == true & IsCheckBoxLenght == false) MessageBox.Show($"Square of the circle = {Math.Round((Math.PI * Math.Pow(Radius, 2)),2)} cm^2", "Calculation");
+            else if (IsCheckBoxSquare == true & IsCheckBoxLenght == true) MessageBox.Show($"Square of the circle = {Math.Round((Math.PI * Math.Pow(Radius, 2)),2)} cm^2 \nLenght of the circle = {Math.Round((2 * Math.PI * Radius),2)} cm", "Calculation");
+        }
+
+        private void btnInput_Click(object sender, RoutedEventArgs e)
+        {
+            Input input = new Input();
+            input.Show();
+            this.Close();
+        }
+
+        private void btnDraw_Click(object sender, RoutedEventArgs e)
+        {
+            if (Radius <= 12)
+            {
+                gridDraw.Visibility = Visibility.Visible;
+                circle.Height = (Radius * 2) * 10;
+                circle.Width = circle.Height;
+            }
+            else MessageBox.Show("The figure cannot be drawn. The radius is too large.");
+        }
+
+        private void winMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (IsCheckBoxSquare == true || IsCheckBoxLenght == true)
+            {
+                btnDraw.IsEnabled = true;
+                btnCalc.IsEnabled = true;
+            }
+            else
+            {
+                btnDraw.IsEnabled = false;
+                btnCalc.IsEnabled = false;
+            }
+        }
+    }
+}
